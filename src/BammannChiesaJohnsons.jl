00cc54ffd216efd019fbf6ce692c404af06ce96d
Joby M. Anthony III <jmanthony1@liberty.edu>
2024-09-25 10:14:41 -0400

    separate back and front end gui functions and widgets

--- src/BammannChiesaJohnsons.jl
+++ src/BammannChiesaJohnsons.jl
@@ -6,4 +6,13 @@ export BCJ_metal_currentconfiguration
 export BCJ_metal_currentconfiguration_init
 export solve!
 
+include("BCJ_GUI.jl")
+export BCJ_metal_calibrate
+export constant_string
+export dataseries_init
+export BCJ_metal_calibrate_kernel
+export BCJ_metal_calibrate_init
+export BCJ_metal_calibrate_update!
+export update!
+
 end
